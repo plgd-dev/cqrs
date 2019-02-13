39189c2f64662cc4d390de96613489aefc8c8884
Jozef Kralik <jozef.kralik@kistler.com>
2019-02-13 09:12:50 +0100

    update comments

--- eventstore/eventstore.go
+++ eventstore/eventstore.go
@@ -6,20 +6,21 @@ import (
 	event "github.com/go-ocf/cqrs/event"
 )
 
-// QueryFromVersion used to load events from snapshot.
+// QueryFromVersion used to load events from version.
 type QueryFromVersion struct {
-	AggregateId string //tlo certain aggregateId
+	AggregateId string //required
 	Version     uint64 //required
 }
 
 // QueryFromSnapshot used to load events from snapshot.
 type QueryFromSnapshot struct {
-	GroupId           string //filter by groupId is used only when aggagateId is empty
+	GroupId           string //filter by group Id and it is used only when aggreagateId is empty
 	AggregateId       string //filter to certain aggregateId
 	SnapshotEventType string //required
 }
 
-// EventStore provides interface over eventstore.
+// EventStore provides interface over eventstore. More aggregates can be grouped by groupId,
+// but aggregateId of aggregates must be unique against whole DB.
 type EventStore interface {
 	Save(ctx context.Context, groupId string, aggregateId string, events []event.Event) (concurrencyException bool, err error)
 	SaveSnapshot(ctx context.Context, groupId string, aggregateId string, event event.Event) (concurrencyException bool, err error)
