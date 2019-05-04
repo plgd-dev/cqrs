c312b10894adac2e102bb9a6bc158594833a25e5
Jozef Kralik <jojo.lwin@gmail.com>
2019-05-04 14:28:11 +0200

    implements nats for eventbus

---
+++ eventbus/nats/publisher.go
@@ -0,0 +1,69 @@
+package nats
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/go-ocf/cqrs/event"
+	protoEventBus "github.com/go-ocf/cqrs/protobuf/eventbus"
+	nats "github.com/nats-io/go-nats"
+)
+
+// Publisher implements a eventbus.Publisher interface.
+type Publisher struct {
+	dataMarshaler event.MarshalerFunc
+	conn          *nats.Conn
+}
+
+// NewPublisher creates a publisher.
+func NewPublisher(url string, eventMarshaler event.MarshalerFunc, options ...nats.Option) (*Publisher, error) {
+	conn, err := nats.Connect(url, options...)
+	if err != nil {
+		return nil, fmt.Errorf("cannot connect to server: %v", err)
+	}
+
+	return &Publisher{
+		dataMarshaler: eventMarshaler,
+		conn:          conn,
+	}, nil
+}
+
+// Publish publishes an event to topics.
+func (p *Publisher) Publish(ctx context.Context, topics []string, groupId, aggregateId string, event event.Event) error {
+	data, err := p.dataMarshaler(event)
+	if err != nil {
+		return errors.New("could not marshal data for event: " + err.Error())
+	}
+
+	e := protoEventBus.Event{
+		EventType:   event.EventType(),
+		Data:        data,
+		Version:     event.Version(),
+		GroupId:     groupId,
+		AggregateId: aggregateId,
+	}
+
+	eData, err := e.Marshal()
+	if err != nil {
+		return errors.New("could not marshal event: " + err.Error())
+	}
+
+	var errors []error
+	for _, t := range topics {
+		err := p.conn.Publish(t, eData)
+		if err != nil {
+			errors = append(errors, err)
+		}
+	}
+	if len(errors) > 0 {
+		return fmt.Errorf("cannot publish events: %v", errors)
+	}
+
+	return nil
+}
+
+// Close close connection to nats
+func (p *Publisher) Close() {
+	p.conn.Close()
+}
